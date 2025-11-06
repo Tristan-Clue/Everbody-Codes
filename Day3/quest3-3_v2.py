9a8a447f4e7c6d394fd17cbde213465c32785468
Tristan <123tristanma@gmail.com>
2025-11-06 18:25:35 +0800

    Added secondary (more efficient) way

---
+++ Day3/quest3-3_v2.py
@@ -0,0 +1,23 @@
+num = [5,4,3,2,2,2,1]
+
+max_num = max(num, key=num.count)
+max_count = num.count(max_num)
+print(max_count)
+#num.sort(reverse=True)
+
+#print(num)
+
+#unique = list(set(num))
+
+#print(unique)
+#unique.sort(reverse=True)
+
+#list_len = len(unique)
+#print(list_len)
+
+#new_unique = unique[list_len - 20:]
+#print(new_unique)
+#total_sum = sum(new_unique)
+
+#print(total_sum)
+
